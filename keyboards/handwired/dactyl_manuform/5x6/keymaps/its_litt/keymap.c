0fbb0e73f070b8dd72df2c18b29c7894e272fb24
jennetters <jennettereid@gmail.com>
2019-05-03 22:44:17 -0400

    final config

--- keyboards/handwired/dactyl_manuform/5x6/keymaps/its_litt/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/its_litt/keymap.c
@@ -23,9 +23,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      KC_CAPS, KC_A  , KC_S  , KC_D  , KC_F  , KC_G  ,                   KC_H  , KC_J  , KC_K  , KC_L  ,KC_SCLN,KC_QUOT,
      KC_LSFT, KC_Z  , KC_X  , KC_C  , KC_V  , KC_B  ,                   KC_N  , KC_M  ,KC_COMM,KC_DOT ,KC_SLSH,KC_ENT,
                       KC_LCTL,KC_LALT,                                                KC_RALT, KC_RCTL,
-                                      KC_LGUI,KC_SPC,               KC_SPC,  LOWER,
-                                      RAISE,  KC_PGUP,              KC_HOME, KC_PSCR,
-                                      RESET,  KC_PGDN,              KC_END,  RESET
+                                      RAISE,KC_SPC,               KC_SPC,  LOWER,
+                                      KC_LGUI,  KC_PGUP,          KC_HOME, KC_PSCR,
+                                      RESET,  KC_PGDN,             KC_END,  RESET
   ),
 
   [_LOWER] = LAYOUT_5x6(
